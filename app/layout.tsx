c5841669af9412677f704307bd019873489e3eaa
Michael-Reis <michael_farias14@hotmail.com>
2025-08-20 00:51:48 -0300

    feat: adicionando title

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,33 @@ const roboto = Roboto({
 })
 
 export const metadata: Metadata = {
-  title: "Pequenos Sonhadores - Pré-Escola e Berçário",
+  title: "Colmeia do saber - Pré-Escola e Berçário",
   description:
     "Pré-escola e berçário com ambiente acolhedor e estimulante. Educação infantil de qualidade com turmas reduzidas e professores capacitados.",
   generator: "v0.app",
+  openGraph: {
+    title: "Colmeia do saber - Pré-Escola e Berçário",
+    description: "Pré-escola e berçário com ambiente acolhedor e estimulante. Educação infantil de qualidade com turmas reduzidas e professores capacitados.",
+    url: "https://colmeiadosaber.com.br/",
+    siteName: "Colmeia do Saber",
+    images: [
+      {
+        url: "https://colmeiadosaber.com.br/logo.png",
+        width: 800,
+        height: 600,
+        alt: "Logo Colmeia do Saber",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Colmeia do saber - Pré-Escola e Berçário",
+    description: "Pré-escola e berçário com ambiente acolhedor e estimulante. Educação infantil de qualidade com turmas reduzidas e professores capacitados.",
+    images: ["https://colmeiadosaber.com.br/logo.png"],
+    site: "@colmeiadosaber",
+  },
 }
 
 export default function RootLayout({
