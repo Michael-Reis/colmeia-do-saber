67fc4072fae9594b9894233247d64391a1cf5f7b
Michael-Reis <michael_farias14@hotmail.com>
2025-08-19 23:39:36 -0300

    feat: adicionando url do instagram

--- components/footer-social.tsx
+++ components/footer-social.tsx
@@ -5,7 +5,9 @@ export function FooterSocial() {
     return (
         <div className="flex gap-4">
             <FaFacebookF size={20} className="cursor-pointer"/>
-            <FaInstagram size={20} className="cursor-pointer"/>
+            <FaInstagram size={20} className="cursor-pointer" onClick={() => {
+                window.open("https://www.instagram.com/colegiocolmeiadosaber/", "_blank");
+            }}/>
             <FaWhatsapp size={20} className="cursor-pointer"/>
         </div>
     )
